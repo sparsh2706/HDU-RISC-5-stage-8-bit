38593244fa94af6c049631be62acfe0460e32619
sparsh2706 <dutta170103065@iitg.ernet.in>
2019-11-10 18:12:02 +0530

    Input Mode Tick

--- input_mode.v
+++ input_mode.v
@@ -1,16 +1,35 @@
-module inp(input_val,instrMem);
+module inp(input_val,instrMem,but_inp);
 	input [7:0] input_val;
-	output [7:0] instrMem[7:0];
+	input but_inp;
+	output reg [63:0] instrMem;
 
-	wire but_inp;
-	reg [7:0] instrMem[7:0];
-
-	integer inp_num;
-	assign inp_num = 0;
 	always @(but_inp) begin
-		assign instrMem[inp_num] = input_val;
-		$display("%b",instrMem[inp_num]);
-		assign inp_num = inp_num + 1;
+		instrMem = {instrMem,input_val};
+		$display("Instruction bits till now are : %b",instrMem);
 	end
 
 endmodule
+
+
+	// reg inp_num;
+	
+	// initial
+	// begin
+	// 	assign inp_num = 3'b000;
+	// end
+	
+	// always @(but_inp) begin
+	// 	assign instr[inp_num] = input_val;
+	// 	$display("%b",instr[inp_num]);
+	// 	assign inp_num = inp_num + 1;
+	// end
+
+	// integer i;
+	// initial
+	// begin
+	// 	for(i=0;i<$size(instr);i = i + 1)
+	// 	begin
+	// 		assign instrMem = {instrMem,instr[i]};
+	// 	end
+	// end
+
--- input_mode_tb.v
+++ input_mode_tb.v
@@ -1,26 +1,36 @@
 module inp_tb();
 
 	 reg [7:0] input_val;
-	 reg [7:0] instrMem[7:0];
+	 reg but_inp;
+	 wire [63:0] instrMem;
 
-	 inp testing (.input_val(input_val),.instrMem(instrMem));
+	 inp testing (.input_val(input_val),.instrMem(instrMem),.but_inp(but_inp));
 
 	 initial begin
+	 	but_inp = 1;
 	 	input_val = 8'b10001000;
+	 	but_inp = 0;
 	 	#1000
 	 	input_val = 8'b10001001;
+	 	but_inp = 1;
 	 	#1000
 	 	input_val = 8'b10001010;
+	 	but_inp = 0;
 	 	#1000
 	 	input_val = 8'b10001100;
+	 	but_inp = 1;
 	 	#1000
 	 	input_val = 8'b10010000;
+	 	but_inp = 0;
 	 	#1000
 	 	input_val = 8'b10101000;
+	 	but_inp = 1;
 	 	#1000
 	 	input_val = 8'b10001001;
+	 	but_inp = 0;
 	 	#1000
 	 	input_val = 8'b11111111;
+	 	but_inp = 1;
 	 	$finish;
 	 end
 
