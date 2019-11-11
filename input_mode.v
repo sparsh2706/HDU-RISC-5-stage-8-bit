85281cfd54f880ac40ac5e840cbd65fa7f9c9e70
sparsh2706 <dutta170103065@iitg.ernet.in>
2019-11-11 19:34:42 +0530

    Hazard Bits Generated

--- input_mode.v
+++ input_mode.v
@@ -25,10 +25,9 @@ endmodule
 
 module check(instrMemBits,hazardMemBits);
 	
-	input instrMemBits;
+	input wire [63:0] instrMemBits;
 	output reg [63:0] hazardMemBits;
 
-	reg slicedbits;
 	reg [7:0] instrMem[7:0];
 	reg [1:0] opc[7:0]; 		//reg0
 	reg [2:0] rs[7:0];			//reg1
@@ -38,82 +37,225 @@ module check(instrMemBits,hazardMemBits);
 	integer i;
 	reg assigned;
 
-	initial
-	begin
-		// slicedbits = instrMemBits[63:56];
-		instrMem[0] <= instrMemBits[63:56];
-		instrMem[1] <= instrMemBits[55:48];
-		instrMem[2] <= instrMemBits[47:40];
-		instrMem[3] <= instrMemBits[39:32];
-		instrMem[4] <= instrMemBits[31:24];
-		instrMem[5] <= instrMemBits[23:16];
-		instrMem[6] <= instrMemBits[15:8];
-		instrMem[7] <= instrMemBits[7:0];
-	end
+	// initial
+	// begin
+	// 	instrMem[0][7:0] = instrMemBits[63:56];
+	// 	instrMem[1][7:0] = instrMemBits[55:48];
+	// 	instrMem[2][7:0] = instrMemBits[47:40];
+	// 	instrMem[3][7:0] = instrMemBits[39:32];
+	// 	instrMem[4][7:0] = instrMemBits[31:24];
+	// 	instrMem[5][7:0] = instrMemBits[23:16];
+	// 	instrMem[6][7:0] = instrMemBits[15:8];
+	// 	instrMem[7][7:0] = instrMemBits[7:0];
+	// end
 
 	always @(*)
 	begin
-		for(i=0;i<$size(instrMem);i = i + 1) 
+// ************************************************ Defining the OpCodes ***************************************************
+		
+		opc[0] = instrMemBits[63:62];
+		opc[1] = instrMemBits[55:54];
+		opc[2] = instrMemBits[47:46];
+		opc[3] = instrMemBits[39:38];
+		opc[4] = instrMemBits[31:30];
+		opc[5] = instrMemBits[23:22];
+		opc[6] = instrMemBits[15:14];
+		opc[7] = instrMemBits[7:6];
+
+// ********************************************** Source Register ***********************************************************
+
+		if(opc[0] != 2'b10)
 		begin
-			opc[i] = instrMem[7:6];		
+			rs[0] = instrMemBits[61:59];
+		end
+		else begin
+			rs[0] = instrMemBits[58:56];
 		end
 
-		for(i=0;i<$size(instrMem);i = i + 1) 
+		if(opc[1] != 2'b10)
 		begin
-			if(opc[i] == noop)
-			begin
-				rs[i] = r1;
-			end
-			else if(opc[i] == sw)
-			begin
-				rs[i] = instrMem[2:0];
-			end
-			else begin
-				rs[i] = instrMem[5:3];
-			end
+			rs[1] = instrMemBits[53:51];
+		end
+		else begin
+			rs[1] = instrMemBits[50:48];
 		end
 
-		for(i=0;i<$size(instrMem);i = i + 1) 
+		if(opc[2] != 2'b10)
 		begin
-			if(opc[i] == sw)
-			begin
-				rd[i] = instrMem[2:0];
-			end
-			else begin
-				rd[i] = instrMem[5:3];
-			end
+			rs[2] = instrMemBits[45:43];
+		end
+		else begin
+			rs[2] = instrMemBits[42:40];
 		end
 
-		for(i=0;i<$size(instrMem) - 2;i = i + 1) 
+		if(opc[3] != 2'b10)
+		begin
+			rs[3] = instrMemBits[37:35];
+		end
+		else begin
+			rs[3] = instrMemBits[34:32];
+		end
+
+		if(opc[4] != 2'b10)
+		begin
+			rs[4] = instrMemBits[29:27];
+		end
+		else begin
+			rs[4] = instrMemBits[26:24];
+		end
+
+		if(opc[5] != 2'b10)
+		begin
+			rs[5] = instrMemBits[21:19];
+		end
+		else begin
+			rs[5] = instrMemBits[18:16];
+		end
+
+		if(opc[6] != 2'b10)
+		begin
+			rs[6] = instrMemBits[13:11];
+		end
+		else begin
+			rs[6] = instrMemBits[10:8];
+		end
+
+		if(opc[7] != 2'b10)
+		begin
+			rs[7] = instrMemBits[5:3];
+		end
+		else begin
+			rs[7] = instrMemBits[2:0];
+		end
+
+// ********************************************** Destination Register *****************************************************
+
+		if(opc[0] == 2'b10)
+		begin
+			rd[0] = instrMemBits[61:59];
+		end
+		else begin
+			rd[0] = instrMemBits[58:56];
+		end
+
+		if(opc[1] == 2'b10)
+		begin
+			rd[1] = instrMemBits[53:51];
+		end
+		else begin
+			rd[1] = instrMemBits[50:48];
+		end
+
+		if(opc[2] == 2'b10)
+		begin
+			rd[2] = instrMemBits[45:43];
+		end
+		else begin
+			rd[2] = instrMemBits[42:40];
+		end
+
+		if(opc[3] == 2'b10)
+		begin
+			rd[3] = instrMemBits[37:35];
+		end
+		else begin
+			rd[3] = instrMemBits[34:32];
+		end
+
+		if(opc[4] == 2'b10)
+		begin
+			rd[4] = instrMemBits[29:27];
+		end
+		else begin
+			rd[4] = instrMemBits[26:24];
+		end
+
+		if(opc[5] == 2'b10)
+		begin
+			rd[5] = instrMemBits[21:19];
+		end
+		else begin
+			rd[5] = instrMemBits[18:16];
+		end
+
+		if(opc[6] == 2'b10)
+		begin
+			rd[6] = instrMemBits[13:11];
+		end
+		else begin
+			rd[6] = instrMemBits[10:8];
+		end
+
+		if(opc[7] == 2'b10)
+		begin
+			rd[7] = instrMemBits[5:3];
+		end
+		else begin
+			rd[7] = instrMemBits[2:0];
+		end
+
+// ********************************************************************************************************************
+
+
+		for(i = 3'b000;i < 3'b110;i = i + 1) 
 		begin
 			
-			if(opc[i] != noop)
+			if(opc[i] != 2'b00)
 			begin		
 				if(rd[i] == rs[i+1])
 				begin
-					assigned = i;
-					hazardMemBits = {hazardMemBits,assigned,assigned+1,3'b01};
+					// assigned = 3'b000 + i;
+					hazardMemBits = {hazardMemBits,i,i+3'b001,2'b01};
 				end
 
-				if(rd[i] == rs[i+2] && opc[i] == add)
+				if(rd[i] == rs[i+2] && opc[i] == 2'b01)
 				begin
-					assigned = i;
-					hazardMemBits = {hazardMemBits,assigned,assigned+2,3'b01};
+					// assigned = 3'b000 + i;
+					hazardMemBits = {hazardMemBits,i,i+3'b010,2'b01};
 				end
 			end
 
 		end
 
-		if(opc[$size(instrMem)] !=  noop)
+		if(opc[6] !=  2'b00)
 		begin
-			if(rd[instrMem[$size(instrMem) - 2]] == rs[instrMem[$size(instrMem) - 1]])
+			if(rd[instrMem[6]] == rs[instrMem[7]])
 			begin
-				assigned = $size(instrMem);
-				hazardMemBits = {hazardMemBits,assigned-2,assigned-1,3'b01};	
+				hazardMemBits = {hazardMemBits,3'b110,3'b111,2'b01};	
 			end
 		end
 	end
 
-	// integer size = $bits(hazardMem);
-
 endmodule
+
+
+		// for(i=0;i<$size(instrMem);i = i + 1) 
+		// begin
+		// 	opc[i] = instrMem[i][7:6];		
+		// end
+
+		// for(i=0;i<$size(instrMem);i = i + 1) 
+		// begin
+		// 	if(opc[i] == noop)
+		// 	begin
+		// 		rs[i] = r1;
+		// 	end
+		// 	else if(opc[i] == sw)
+		// 	begin
+		// 		rs[i] = instrMem[i][2:0];
+		// 	end
+		// 	else begin
+		// 		rs[i] = instrMem[i][5:3];
+		// 	end
+		// end
+
+		// for(i=0;i<$size(instrMem);i = i + 1) 
+		// begin
+		// 	if(opc[i] == sw)
+		// 	begin
+		// 		rd[i] = instrMem[i][2:0];
+		// 	end
+		// 	else begin
+		// 		rd[i] = instrMem[i][5:3];
+		// 	end
+		// end
--- input_mode_tb.v
+++ input_mode_tb.v
@@ -31,14 +31,21 @@ module inp_tb();
 	 	input_val = 8'b10001001;
 	 	but_inp = 0;
 	 	#1000
-	 	// input_val = 8'b11111111;
-	 	// but_inp = 1;
-	 	// #1000
-		$display("Instruction bits Finally are : %d",$bits(instrMemBits));
+	 	input_val = 8'b11111111;
+	 	but_inp = 1;
+	 	#1000
+		// $display("Instruction bits Finally are : %d",$bits(instrMemBits));
 		#1000
+	 	
 	 	$finish;
 	 end
+	 	check Checking (.instrMemBits(instrMemBits),.hazardMemBits(hazardMemBits));
 
-	 check Checking (.instrMemBits(instrMemBits),.hazardMemBits(hazardMemBits));
+	 	initial
+	 	begin
+	 		#9000
+	 		$display("Hazard Bits are : %b",hazardMemBits);
+	 	end
+
 
 endmodule
